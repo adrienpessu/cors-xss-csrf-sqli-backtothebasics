3492c163fabea88f997d817a9efdc6343df44a3d
Adrien Pessu <adrien@pessu.net>
2018-12-29 20:54:24 +0100

    add react xss

---
+++ react-xss/app/containers/FeaturePage/index.js
@@ -0,0 +1,92 @@
+/*
+ * FeaturePage
+ *
+ * List all the features
+ */
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { FormattedMessage } from 'react-intl';
+
+import H1 from 'components/H1';
+import messages from './messages';
+import List from './List';
+import ListItem from './ListItem';
+import ListItemTitle from './ListItemTitle';
+
+export default class FeaturePage extends React.Component {
+  // Since state and props are static,
+  // there's no need to re-render this component
+  shouldComponentUpdate() {
+    return false;
+  }
+
+
+  render() {
+    function createMarkup() { return {__html: this.props.match.params.name}; };
+    return (
+      <div>
+        <Helmet>
+          <title>Feature Page</title>
+          <meta
+            name="description"
+            content="Feature page of React.js Boilerplate application"
+          />
+        </Helmet>
+        <H1>
+          <FormattedMessage {...messages.header} />
+        </H1>
+        <div>name : {this.props.match.params.name}</div>
+        <div
+          dangerouslySetInnerHTML={{
+            __html : `${this.props.match.params.name}`,
+          }} />
+        <List>
+          <ListItem>
+            <ListItemTitle>
+              <FormattedMessage {...messages.scaffoldingHeader} />
+            </ListItemTitle>
+            <p>
+              <FormattedMessage {...messages.scaffoldingMessage} />
+            </p>
+          </ListItem>
+
+          <ListItem>
+            <ListItemTitle>
+              <FormattedMessage {...messages.feedbackHeader} />
+            </ListItemTitle>
+            <p>
+              <FormattedMessage {...messages.feedbackMessage} />
+            </p>
+          </ListItem>
+
+          <ListItem>
+            <ListItemTitle>
+              <FormattedMessage {...messages.routingHeader} />
+            </ListItemTitle>
+            <p>
+              <FormattedMessage {...messages.routingMessage} />
+            </p>
+          </ListItem>
+
+          <ListItem>
+            <ListItemTitle>
+              <FormattedMessage {...messages.networkHeader} />
+            </ListItemTitle>
+            <p>
+              <FormattedMessage {...messages.networkMessage} />
+            </p>
+          </ListItem>
+
+          <ListItem>
+            <ListItemTitle>
+              <FormattedMessage {...messages.intlHeader} />
+            </ListItemTitle>
+            <p>
+              <FormattedMessage {...messages.intlMessage} />
+            </p>
+          </ListItem>
+        </List>
+      </div>
+    );
+  }
+}
